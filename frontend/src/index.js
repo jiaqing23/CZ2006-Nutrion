e200525486799630d78c51930749658eb211827f
wjwen5 <jwong097@e.ntu.edu.sg>
2021-03-08 13:55:46 +0800

    Add font and nav bar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
 
+//import "bootstrap/dist/css/bootstrap.min.css";
+//import "./styles/bootstrap-4.0.0.css";
+
 ReactDOM.render(
-    <App />,
+    <Router>
+       {/* <Auth0ProviderWithHistory> */}
+            <App />
+       {/*  </Auth0ProviderWithHistory>*/}
+    </Router>,
   document.getElementById('root')
 );
