726303ca2341827889047a3d59c7a9909e216403
yuanharnteo <bryson130191@gmail.com>
2021-03-26 13:13:09 +0800

    pulling newest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
-import Homepage from './pages/Homepage';
+// import Homepage from './pages/Homepage';
 import Navigation from './components/Navbar';
+import Profile from './pages/Profile';
 // eslint-disable-next-line
-import Registration from './pages/Registration';
-import Login from './pages/Login'
-import MealPlanner from './components/MealPlanner';
-import MealPlannerBoard from './components/MealPlannerBoard'
-import SearchResult from './pages/SearchResult';
+// import Registration from './pages/Registration';
+// import Login from './pages/Login'
+// import MealPlanner from './components/MealPlanner';
+// import MealPlannerBoard from './components/MealPlannerBoard'
+// import SearchResult from './pages/SearchResult';
 
 function App() {
   return (
     <div>
-      <Homepage />
       <Navigation />
+      {/* <Homepage /> */}
       {/* <Registration /> */}
       {/* <Login /> */}
       <Profile />
