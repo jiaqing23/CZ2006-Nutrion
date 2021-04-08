87ae60f9c188aa4938be6482f43c505f7226c42d
wjwen5 <jwong097@e.ntu.edu.sg>
2021-04-08 22:40:22 +0800

    Fix favourite tab bugs and infinite bg imgae

--- frontend/src/components/FavouriteComponent.jsx
+++ frontend/src/components/FavouriteComponent.jsx
@@ -5,7 +5,7 @@ import y from '../assets/images/salmon.jpg'
 export default class FavouriteComponent extends Component {
     render() {
         return (
-            <div>
+            <div style={{height:"fit-content"}}>
                 <div className="row favrecipeBox">
                 <div style={{ padding: 0, borderRadius: "25px" }} className="col-6">
                     <img className="favrecipeImg" src={y} alt="None" />
--- frontend/src/components/MealPlanner.jsx
+++ frontend/src/components/MealPlanner.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/MealPlanner.css'
 import { GiPieChart, GiHotMeal } from "react-icons/gi";
 import MealPlannerBoard from './MealPlannerBoard'
@@ -7,17 +7,17 @@ import FavouriteComponent from './FavouriteComponent'
 import MealPlannerChart from './MealPlannerChart'
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-var recipes = [{recipeTitle:"Bread",recipeImage:"../assets/images/slider/2.jpg",recipeCalories:"230kcal",recipeTime:"45minutes"},
-            {recipeTitle:"Liu Sha Bao",recipeImage:"../assets/images/slider/3.jpg",recipeCalories:"0kcal",recipeTime:"5minutes"},
-            {recipeTitle:"Liu Sha Bao",recipeImage:"../assets/images/slider/3.jpg",recipeCalories:"0kcal",recipeTime:"5minutes"},
-            {recipeTitle:"Liu Sha Bao",recipeImage:"../assets/images/slider/3.jpg",recipeCalories:"0kcal",recipeTime:"5minutes"}];
+var recipes = [{ recipeTitle: "Bread", recipeImage: "../assets/images/slider/2.jpg", recipeCalories: "230kcal", recipeTime: "45minutes" },
+{ recipeTitle: "Bread", recipeImage: "../assets/images/slider/2.jpg", recipeCalories: "230kcal", recipeTime: "45minutes" },
+{ recipeTitle: "Bread", recipeImage: "../assets/images/slider/2.jpg", recipeCalories: "230kcal", recipeTime: "45minutes" },
+{ recipeTitle: "Liu Sha Bao", recipeImage: "../assets/images/slider/3.jpg", recipeCalories: "0kcal", recipeTime: "5minutes" }];
 function MealPlanner() {
     const [selected, setSelected] = useState("mealplan");
     const [selected2, setSelected2] = useState("recommendation");
-    
+
     //Toggle for planning board - meal plan and nutrient
     const toggleToMealPlan = (e) => {
-        setSelected("mealplan"); 
+        setSelected("mealplan");
     }
 
     const toggleToNutrient = (e) => {
@@ -26,46 +26,46 @@ function MealPlanner() {
 
     //Toggle for left panel - recommendation and favourite panel
     const toggleToRecommdation = (e) => {
-        setSelected2("recommendation"); 
+        setSelected2("recommendation");
     }
 
     const toggleToFavourite = (e) => {
         setSelected2("favourite");
     }
 
-    return(
+    return (
         <div class="wrapMP">
             <div class="left">
                 <h4>Favourite Dishes</h4>
-                <div className="FavouriteList d-flex flex-wrap justify-content-center">
-              {
-              recipes.map((recipe) =>
-                (<FavouriteComponent
-                  recipeTitle={recipe.recipeTitle}
-                  recipeImage={recipe.recipeImage}
-                  recipeCalories={recipe.recipeCalories}
-                  recipeTime={recipe.recipeTime}
-                />))}
+                <div className="FavouriteList justify-content-center">
+                    {
+                        recipes.map((recipe) =>
+                        (<FavouriteComponent
+                            recipeTitle={recipe.recipeTitle}
+                            recipeImage={recipe.recipeImage}
+                            recipeCalories={recipe.recipeCalories}
+                            recipeTime={recipe.recipeTime}
+                        />))}
+                </div>
             </div>
-            </div>
-            
+
             <div class="right">
                 <div class="button-row justify-content-end">
                     <div class="d-flex flex-row-reverse">
-                        <Button text="Save"/>
+                        <Button text="Save" />
                         <Button text="Print" />
                     </div>
                 </div>
-                
+
                 <ul className="tabs group">
-                    <li className={selected=="mealplan"?"active":""}> <a href="#one" onClick={toggleToMealPlan}> <GiHotMeal /> Meal Plan </a></li> 
-                    <li className={selected=="nutrient"?"active":""}> <a href="#two" onClick={toggleToNutrient}> <GiPieChart /> Nutrient </a></li>
+                    <li className={selected == "mealplan" ? "active" : ""}> <a href="#one" onClick={toggleToMealPlan}> <GiHotMeal /> Meal Plan </a></li>
+                    <li className={selected == "nutrient" ? "active" : ""}> <a href="#two" onClick={toggleToNutrient}> <GiPieChart /> Nutrient </a></li>
                 </ul>
-        
-            <div id="content">
-                 {selected=="mealplan"?<MealPlannerBoard/>:<MealPlannerChart/>} 
- 
-            </div>
+
+                <div id="content">
+                    {selected == "mealplan" ? <MealPlannerBoard /> : <MealPlannerChart />}
+
+                </div>
             </div>
         </div>
     );
